1c03c690d0d9419200217a4ecaae67cd59d8be6d
Asel Williams <aselwilliams22@gmail.com>
2022-12-07 23:09:34 -0600

    adds subqueries file

--- server/controller.js
+++ server/controller.js
@@ -15,7 +15,7 @@ let nextEmp = 5
 
 module.exports = {
     getUpcomingAppointments: (req, res) => {
-        sequelize.query(`select a.appt_id, a.date, a.service_type, a.approved, a.completed, u.first_name, u.last_name 
+        sequelize.query(`select a.appt_id, a.date, a.service_type, a.approved,a.notes, a.completed, u.first_name, u.last_name 
         from cc_appointments a
         join cc_emp_appts ea on a.appt_id = ea.appt_id
         join cc_employees e on e.emp_id = ea.emp_id
@@ -33,7 +33,6 @@ module.exports = {
         UPDATE cc_appointments 
         SET approved = true
         WHERE appt_id = ${apptId};
-
         insert into cc_emp_appts (emp_id, appt_id)
         values (${nextEmp}, ${apptId}),
         (${nextEmp + 1}, ${apptId});
